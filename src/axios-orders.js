e57914a75929f7c1e1046e829e3d3937ec3340fd
emrexhepi <em.rexhepi@gmail.com>
2018-09-12 11:57:14 +0200

    global error handling

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://reactjs-burger-builder-1.firebaseio.com/'
+    baseURL: 'https://reactjs-burger-builder-1.firebaseio.com'
 })
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 // import axios instance
 import axios from '../../axios-orders';
@@ -86,7 +87,7 @@ class BurgerBuilder extends Component {
                 }
             }
 
-            await axios.post('/orders.json', order);
+            await axios.post('/orders', order);
             
             this.setState({loadig: false, ordering: false});
         } catch (e) {
@@ -187,4 +188,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
